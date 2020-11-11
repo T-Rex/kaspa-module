bb5b05667d20a988c1f45b04ba599b9234699334
denis.s <denis.s@admins-Mac-mini-2.local>
2020-11-11 13:02:57 +0200

    added apiHelpers export

--- index.ts
+++ index.ts
@@ -1,7 +1,8 @@
 import Wallet from "./wallet/Wallet";
+import * as apiHelpers from './wallet/apiHelpers'
 import bitcoreKaspaSetup from './wallet/bitcoreKaspaSetup';
 
 // module.exports.Wallet = Wallet;
 // module.exports.bitcoreKaspaSetup = bitcoreKaspaSetup;
 
-export default {Wallet, bitcoreKaspaSetup}
+export default {Wallet, bitcoreKaspaSetup, apiHelpers}
