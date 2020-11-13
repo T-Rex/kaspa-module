6444c9416b434132d283dc22b125a966eccd37ee
denis.s <denis.s@admins-Mac-mini-2.local>
2020-11-13 14:53:04 +0200

    change exporting functional

--- index.ts
+++ index.ts
@@ -4,5 +4,8 @@ import bitcoreKaspaSetup from './wallet/bitcoreKaspaSetup';
 
 // module.exports.Wallet = Wallet;
 // module.exports.bitcoreKaspaSetup = bitcoreKaspaSetup;
+exports.Wallet = Wallet;
+exports.bitcoreKaspaSetup = bitcoreKaspaSetup;
+exports.apiHelpers = apiHelpers;
 
-export default {Wallet, bitcoreKaspaSetup, apiHelpers}
+// export default {Wallet, bitcoreKaspaSetup, apiHelpers}
