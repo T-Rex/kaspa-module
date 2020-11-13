4a59eb10fc70cdc3671bf484c34ab76dd0764e20
denis.s <denis.s@admins-Mac-mini-2.local>
2020-11-13 15:44:19 +0200

    change module type

--- dist/index.d.ts
+++
@@ -1,7 +0,0 @@
-import Wallet from "./wallet/Wallet";
-declare const _default: {
-    bitcoreKaspaSetup: () => void;
-    Wallet: typeof Wallet;
-};
-export default _default;
-//# sourceMappingURL=index.d.ts.map
